1b1fbb066dc88b2ba552586b9d291db11fb819ae
Базарнов Роман <bazarnoffroman@yandex.ru>
2026-06-22 14:21:10 +0300

    feat(scrapper): add batching and thread pool properties in ScrapperProperties

--- scrapper/src/main/java/chikagebb/linktracker/scrapper/properties/ScrapperProperties.java
+++ scrapper/src/main/java/chikagebb/linktracker/scrapper/properties/ScrapperProperties.java
@@ -1,6 +1,10 @@
 package chikagebb.linktracker.scrapper.properties;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +18,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @AllArgsConstructor
 public class ScrapperProperties {
 
-    @NotNull
+    @NotNull(message = "Размер пакета (batchSize) должен быть указан")
+    @Positive(message = "Размер пакета должен быть больше 0")
     private Integer batchSize;
+
+    @NotEmpty(message = "Интервал запуска (interval) не должен быть пустым")
+    private String interval;
+
+    @NotNull(message = "Количество потов должно быть указано")
+    @Min(value = 1, message = "Количество потоков должно быть не меньше 1")
+    @Max(value = 200, message = "Количество потоков не должно превышать 200")
+    private Integer threadCount;
 }
