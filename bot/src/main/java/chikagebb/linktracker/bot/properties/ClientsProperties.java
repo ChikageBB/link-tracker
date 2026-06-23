1c0e3c92ad338a496b15312d3d919769c69256eb
Базарнов Роман <bazarnoffroman@yandex.ru>
2026-06-23 14:11:09 +0300

    test(scrapper): add github and stackoverflow link checker tests

--- bot/src/main/java/chikagebb/linktracker/bot/properties/ClientsProperties.java
+++ bot/src/main/java/chikagebb/linktracker/bot/properties/ClientsProperties.java
@@ -1,10 +1,30 @@
 package chikagebb.linktracker.bot.properties;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "clients")
-public record ClientsProperties(ClientProperties bot, ClientProperties scrapper) {
+public record ClientsProperties(
+        @Valid @NotNull(message = "Настройки бота не должны быть пустыми")
+        ClientProperties bot,
 
-    public record ClientProperties(String baseUrl, Duration connectTimeout, Duration readTimeout) {}
+        @Valid @NotNull(message = "Настройки скраппера не должны быть пустыми")
+        ClientProperties scrapper) {
+
+    public record ClientProperties(
+            @NotBlank(message = "Базовый URL (baseUrl) не должен быть пустым")
+            @URL(message = "Некорректный формат URL-адреса")
+            String baseUrl,
+
+            @NotNull(message = "connectTimeout должен быть указан")
+            Duration connectTimeout,
+
+            @NotNull(message = "readTimeout должен быть указан")
+            Duration readTimeout) {}
 }
